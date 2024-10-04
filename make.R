2a819b6e83cff04791199157c96009127b70d146
Guilherme S. Grittz <ggrittz@outlook.com>
2024-10-04 20:13:48 -0300

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' Inventorying: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Guilherme Grittz \email{ggrittz@usp.br}
+#' 
+#' @date 2024/10/04
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
